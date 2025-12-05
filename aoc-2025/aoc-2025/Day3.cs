1693b76f7ce836877764f90f9b3c48dbf0589406
tardis2snej <47867092+tardis2snej@users.noreply.github.com>
2025-12-05 01:06:23 +0000

    Day 3 Part 1

---
+++ aoc-2025/aoc-2025/Day3.cs
@@ -0,0 +1,60 @@
+﻿using System.Text.RegularExpressions;
+
+namespace aoc_2025;
+
+public class Day3
+{
+	public void Run(string input)
+	{
+		string text = File.ReadAllText(input);
+		string[] batteries = Regex.Split(text, "\r\n|\r|\n");
+		
+		long sum = 0;
+
+		for (long i = 0; i < batteries.Length; i++)
+		{
+			int[] numbers = ParseLine(batteries[i]);
+			sum += FindBiggestCombo(numbers);
+		}
+		
+		Console.WriteLine($"Sum is {sum}");
+	}
+
+	private int[] ParseLine(string line)
+	{
+		int[] numbers = new int[line.Length];
+
+		for (int i = 0; i < line.Length; i++)
+		{
+			numbers[i] = int.Parse(line[i].ToString());	
+		}
+
+		return numbers;
+	}
+
+	private int FindBiggestCombo(int[] numbers)
+	{
+		int biggestFirstPtr = 0;
+
+		for (int i = 0; i < numbers.Length - 1; i++)
+		{
+			if (numbers[i] > numbers[biggestFirstPtr])
+			{
+				biggestFirstPtr = i;
+			}
+		}
+
+		int biggestSecondPtr = biggestFirstPtr + 1;
+
+		for (int i = biggestSecondPtr; i < numbers.Length; i++)
+		{
+			if (numbers[i] > numbers[biggestSecondPtr])
+			{
+				biggestSecondPtr = i;
+			}
+		}
+		
+		Console.WriteLine($"{numbers[biggestFirstPtr] * 10 + numbers[biggestSecondPtr]}");
+		return numbers[biggestFirstPtr] * 10 + numbers[biggestSecondPtr];
+	}
+}
--- aoc-2025/aoc-2025/Program.cs
+++ aoc-2025/aoc-2025/Program.cs
@@ -7,7 +7,12 @@ using aoc_2025;
 // day1.Run("D:\\Projects\\advent-of-code-2025\\aoc-2025\\aoc-2025\\inputs\\Day1\\input.txt");
 // Console.WriteLine("-----------");
 
-Console.WriteLine("---DAY 2---");
-Day2 day2 = new Day2();
-day2.Run("D:\\Projects\\advent-of-code-2025\\aoc-2025\\aoc-2025\\inputs\\Day2\\input.txt");
+// Console.WriteLine("---DAY 2---");
+// Day2 day2 = new Day2();
+// day2.Run("D:\\Projects\\advent-of-code-2025\\aoc-2025\\aoc-2025\\inputs\\Day2\\input.txt");
+// Console.WriteLine("-----------");
+
+Console.WriteLine("---DAY 3---");
+Day3 day3 = new Day3();
+day3.Run("D:\\Projects\\advent-of-code-2025\\aoc-2025\\aoc-2025\\inputs\\Day3\\input.txt");
 Console.WriteLine("-----------");
