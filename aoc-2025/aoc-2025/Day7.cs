41bbd35cb53b163d3a20f98805e064b25d2c0e8b
tardis2snej <47867092+tardis2snej@users.noreply.github.com>
2025-12-07 16:57:22 +0000

    Day 7 part 2

--- aoc-2025/aoc-2025/Day7.cs
+++ aoc-2025/aoc-2025/Day7.cs
@@ -7,19 +7,29 @@ public class Day7
 	private const char BEAM = '|';
 	private const char SPACE = '.';
 	
+	private const int ENTRY_INT = 1;
+	private const int SPLITTER_INT = -1;
+	private const int SPACE_INT = 0;
+	
 	public void Run(string input)
 	{
 		string text = File.ReadAllText(input);
 		string[] lines = text.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries);
-		char[][] map = ParseInput(lines);
+		long[][] map = ParseInputAsNum(lines);
 
-		int startLoc = Array.IndexOf(map[0], ENTRY);
-		map[0][startLoc] = BEAM;
-		int count = TraceBeam(ref map, 0, startLoc);
+		CalculateTimelines(ref map);
+		
+		int lastLine = map.Length - 1;
+		long count = 0;
+
+		for (int i = 0; i < map[lastLine].Length; i++)
+		{
+			count += map[lastLine][i];
+		}
 		
 		PrintMap(map);
 		
-		Console.WriteLine($"Total splits: {count}");
+		Console.WriteLine($"Total timelines: {count}");
 	}
 
 	private char[][] ParseInput(string[] lines)
@@ -33,6 +43,34 @@ public class Day7
 			for (int j = 0; j < lines[i].Length; j++)
 			{
 				map[i][j] = lines[i][j];
+			}
+		}
+
+		return map;
+	}
+	
+	private long[][] ParseInputAsNum(string[] lines)
+	{
+		long[][] map = new long[lines.Length][];
+		
+		for (int i = 0; i < lines.Length; i++)
+		{
+			map[i] = new long[lines[i].Length];
+
+			for (int j = 0; j < lines[i].Length; j++)
+			{
+				if (lines[i][j] == ENTRY)
+				{
+					map[i][j] = ENTRY_INT;
+				}
+				else if (lines[i][j] == SPLITTER)
+				{
+					map[i][j] = SPLITTER_INT;
+				}
+				else if (lines[i][j] == SPACE)
+				{
+					map[i][j] = SPACE_INT;
+				}
 			}
 		}
 
@@ -75,15 +113,42 @@ public class Day7
 
 		return splitCount;
 	}
-	
-	private void PrintMap(char[][] map)
+
+	private void CalculateTimelines(ref long[][] map)
 	{
-		for (int i = 0; i < map.Length; i++)
+		for (int i = 1; i < map.Length; i++)
 		{
 			for (int j = 0; j < map[i].Length; j++)
 			{
-				Console.Write(map[i][j]);
+				if (map[i][j] > SPLITTER_INT && map[i - 1][j] != SPLITTER_INT) // direct beam
+				{
+					map[i][j] += map[i - 1][j];
+				}
+				else if (map[i][j] == SPLITTER_INT)
+				{
+					if (j > 0) // upd left side
+					{
+						map[i][j - 1] += map[i - 1][j];
+					}
+					
+					if (j < map[i].Length - 1) // upd right side
+					{
+						map[i][j + 1] += map[i - 1][j];
+					}
+				}
 			}
+		}
+	}
+	
+	private void PrintMap<T>(T[][] map)
+	{
+		foreach (var i in map)
+		{
+			foreach (var j in i)
+			{
+				Console.Write(j + "\t");
+			}
+
 			Console.Write("\n");
 		}
 	}
